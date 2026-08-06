da8f13cada3a39f669e4c8cccb6c8a02763cf6de
iamabrar777-beep <iamabrar777@gmail.com>
2026-08-06 16:18:10 +0600

    Hide footer on homepage

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,13 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Footer() {
+  const pathname = usePathname();
+
+  if (pathname === '/') return null;
+
   return (
     <footer className="border-t border-line mt-24">
       <div className="max-w-7xl mx-auto px-5 md:px-8 py-10 flex flex-col md:flex-row items-center justify-between gap-4">
